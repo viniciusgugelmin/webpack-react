0b48e02d9565048eae89139d44744b874449c626
Vinicius <vinigugelmin@hotmail.com>
2021-12-21 12:13:50 -0300

    Webpack init, prettier, eslint, babel, react simplified

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import React from 'react';
+import createReactClass from 'create-react-class';
+
+var Title = createReactClass({
+  render: function () {
+    return <h1>Titulo</h1>;
+  },
+});
+
+export default Title;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+import { render } from 'react-dom';
+import Title from './app';
+
+render(<Title />, document.querySelector('[data-js="app"]'));
